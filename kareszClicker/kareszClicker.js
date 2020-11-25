49fa14d3c9c4e26c7262266a7f39d32e5edfe672
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2020-11-25 21:37:14 +0100

    Update kareszClicker.js

--- kareszClicker/kareszClicker.js
+++ kareszClicker/kareszClicker.js
@@ -1,8 +1,9 @@
-var kareszCount = 1;
+var kareszCount = 0;
 var box;
 var kareszCountText;
 var kareszImage
 var kpc = 1;
+var kps = 0;
 
 function onPageLoad() {
   box = document.querySelector("#mainContainer");
@@ -31,18 +32,18 @@ function saveGame() {
 }
 function loadGame(shouldAsk) {
   if (shouldAsk) {
-    if(confirm(`Are you sure you want to load your save with ${parseInt(localStorage.getItem("kareszCount"))} Karesz'`)) {
+    if(!confirm(`Are you sure you want to load your save with ${parseInt(localStorage.getItem("kareszCount"))} Karesz'`)) {
       return;
     }
   }
-  if (localStorage.getItem("saved") == "true") {
-    kareszCount = parseInt(localStorage.getItem("kareszCount"));
-  }
+  kareszCount = parseInt(localStorage.getItem("kareszCount"));
   update();
 }
 function resetGame() {
   if(confirm(`Are you sure you want to reset?\nYou will lose all ${kareszCount} of you Karesz'`)) {
-  localStorage.clear();
-  loadGame(false);
+    localStorage.clear();
+    kareszCount = 0;
+    saveGame();
+    loadGame(false);
   }
 }
