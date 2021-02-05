4c4489c16a34a93e9361e787c4b2deb166420353
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2021-02-05 22:04:39 +0100

    FINALLY, A NORMAL PURCHASE SYSTEm

--- kareszClicker/index.js
+++ kareszClicker/index.js
@@ -1,78 +1,176 @@
-
-var kareszCount = 0;
-var kareszContainer;
-var sidebar;
-var kareszCountText;
-var kareszPerSecText;
-var kareszImage
-var kareszGeneratorPriceText;
-var kpc = 1;
-var kps = 0;
-var kareszGeneratorPrice = 50
-
-function onPageLoad() {
-    kareszContainer = document.querySelector("#kareszContainer");
-    sidebar = document.querySelector("#sidebar");
-    kareszCountText = kareszContainer.querySelector("#kareszCountText")
-    kareszPerSecText = kareszContainer.querySelector("#kareszPerSecText")
-    kareszImage = kareszContainer.querySelector("#kareszImage")
-    kareszGeneratorPriceText = sidebar.querySelector("#kareszGeneratorPriceText")
-    if (localStorage.getItem("saved") != "true") {
-        saveGame();
+(()=> {
+    class Item {
+        constructor(id, itemText, price, multiplier, kps, kpc, criteria) {
+            this.id = id;
+            this.itemText = itemText
+            this.price = price;
+            this.multiplier = multiplier;
+            this.kps = kps;
+            this.kpc = kpc;
+            this.criteria = criteria;
+        }
+        unlocked = false;
+        count = 0;
     }
-    loadGame(false);
-    setInterval(tick, 1000)
-}
 
-function tick() {
-    kareszCount += kps
-    update()
-}
-function buyKareszGenerator(num) {
-    for (var i = 0; i < num && kareszGeneratorPrice <= kareszCount; i++) {
-        kareszCount -= kareszGeneratorPrice
-        kps += 1
-        kareszGeneratorPrice *= 2;
-        update();
+    function defaultItems() {
+        return [
+            new Item("kareszGenerator", "Karesz Generator", 50, 1.5, 1, 0, function () { return true }),
+            new Item("kareszFactory", "Karesz Factory", 600, 1.6, 10, 0, function () { return items[0].count >= 3 })
+        ]
     }
-}
-function earn(amount) {
-    kareszCount += amount
-    update();
-}
+    var items = defaultItems();
+    var kareszCount = 0;
+    var sidebar;
+    var kareszCountText;
+    var kareszPerSecText;
+    var kareszImage
+    var kpc = 1;
+    var kps = 0;
 
-function saveGame() {
-    localStorage.setItem("saved", true);
-    localStorage.setItem("kareszCount", kareszCount);
-    localStorage.setItem("kps", kps);
-}
-function kareszClicked() {
-    earn(kpc);
-}
+    window.onload = onPageLoad;
 
-function resetGame() {
-    if (confirm(`Are you sure you want to reset?\nYou will lose all ${kareszCount} of you Karesz'`)) {
-        localStorage.clear();
-        kareszCount = 0;
-        kps = 0;
-        saveGame();
+    function onPageLoad() {
+        kareszCountText = document.getElementById("kareszCountText")
+        sidebar = document.getElementById("sidebar");
+        kareszPerSecText = document.getElementById("kareszPerSecText")
+        kareszImage =
+
+            document.getElementById('saveButton').addEventListener("click", () => saveGame());
+        document.getElementById('loadButton').addEventListener("click", () => loadGame(true));
+        document.getElementById('resetButton').addEventListener("click", () => resetGame());
+        document.getElementById("kareszImage").addEventListener("click", () => kareszClicked());
+        if (localStorage.getItem('version') != '1.1') {
+            localStorage.setItem("saved", "false")
+        }
+        localStorage.setItem("version", "1.1")
+        if (localStorage.getItem("saved") != "true") {
+            saveGame();
+        }
         loadGame(false);
+        setInterval(tick, 1000)
     }
-}
 
-function loadGame(shouldAsk) {
-    if (shouldAsk) {
-        if (!confirm(`Are you sure you want to load your save with ${parseInt(localStorage.getItem("kareszCount"))} Karesz'`)) {
-            return;
+    function tick() {
+        kareszCount += kps
+        update()
+    }
+    function buyItem(id, num) {
+        var item = items[id];
+        for (var i = 0; i < num && item.price <= kareszCount; i++) {
+            kareszCount -= item.price
+            item.count++;
+            kps += item.kps
+            kpc += item.kpc
+            item.price = Math.floor(item.price * item.multiplier);
+            update();
         }
     }
-    kareszCount = parseInt(localStorage.getItem("kareszCount"));
-    kps = parseInt(localStorage.getItem("kps"));
-    update();
-}
+    function earn(amount) {
+        kareszCount += amount
+        update();
+    }
 
-function update() {
-    kareszCountText.innerHTML = `Karesz count: ${kareszCount}`
-    kareszPerSecText.innerHTML = `${kps} Karesz/sec`
-    kareszGeneratorPriceText.innerHTML = "Price: " + kareszGeneratorPrice; + " Karesz"
-}
+    function saveGame() {
+        localStorage.setItem("saved", "true");
+        localStorage.setItem("kareszCount", kareszCount);
+        localStorage.setItem("kps", kps);
+        localStorage.setItem("kpc", kpc);
+        localStorage.setItem("items", JSON.stringify(items))
+    }
+    function kareszClicked() {
+        earn(kpc);
+    }
+
+    function resetGame() {
+        if (confirm(`Are you sure you want to reset?\nYou will lose all ${kareszCount} of you Karesz'`)) {
+            localStorage.clear();
+            kareszCount = 0;
+            kps = 0;
+            items = defaultItems();
+            saveGame();
+            loadGame(false);
+        }
+    }
+
+    function loadGame(shouldAsk) {
+        if (shouldAsk) {
+            if (!confirm(`Are you sure you want to load your save with ${parseInt(localStorage.getItem("kareszCount"))} Karesz'`)) {
+                return;
+            }
+        }
+        kareszCount = parseInt(localStorage.getItem("kareszCount"));
+        kps = parseInt(localStorage.getItem("kps"));
+        kpc = parseInt(localStorage.getItem("kpc"));
+        var temp = JSON.parse(localStorage.getItem("items"))
+        for (var i = 0; i < items.length; i++) {
+            items[i].price = temp[i].price
+            items[i].count = temp[i].count
+            items[i].unlocked = temp[i].unlocked
+        }
+        for (var i = 0; i < items.length; i++) {
+            removeItemFromSidebar(i);
+        }
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].unlocked) {
+                addItemToSidebar(i)
+            }
+        }
+        update();
+    }
+
+    function update() {
+        kareszCountText.innerHTML = `Karesz count: ${kareszCount}`
+        kareszPerSecText.innerHTML = `${kps} Karesz/sec`
+        if (sidebar instanceof Element) {
+            for (var i = 0; i < items.length; i++) {
+                var item = items[i]
+                if (item.unlocked) {
+                    var itemH = sidebar.querySelector(`#${item.id}`)
+                    var itemPriceText = itemH.querySelector(".itemPriceText")
+                    itemPriceText.innerHTML = `Price: ${item.price}`
+                } else if (item.criteria()) {
+                    addItemToSidebar(i)
+                    update() //recursion, recursion go away, come again another day //// recursion should not happen
+                }
+            }
+        }
+
+    }
+
+    function addItemToSidebar(id) {
+        var item = items[id]
+        item.unlocked = true;
+        var sidebar = document.getElementById("sidebar");
+
+        var itemH = document.createElement("div");
+        itemH.className = "item";
+        itemH.id = item.id;
+
+        var itemText = document.createElement("div");
+        itemText.className = "itemText"
+        itemText.innerHTML = item.itemText;
+
+        var itemButton = document.createElement("button")
+        itemButton.className = "itemButton"
+        itemButton.innerHTML = "Buy 1"
+        itemButton.addEventListener("click", () => buyItem(id, 1));
+
+        var itemPriceText = document.createElement("div")
+        itemPriceText.className = "itemPriceText"
+        itemPriceText.innerHTML = "Price: "
+
+        itemH.appendChild(itemText);
+        itemH.appendChild(itemButton);
+        itemH.appendChild(itemPriceText);
+        sidebar.appendChild(itemH);
+    }
+    function removeItemFromSidebar(id) {
+        var item = items[id]
+        var sidebar = document.querySelector("#sidebar");
+        var itemH = sidebar.querySelector(`#${item.id}`);
+        if (itemH != null) {
+            itemH.remove();
+        }
+    }
+})()
