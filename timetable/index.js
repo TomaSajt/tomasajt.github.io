37a6d6b33e7762eb3b7d17573a4a4456cc95d505
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2021-03-10 07:59:43 +0100

    a

--- timetable/index.js
+++ timetable/index.js
@@ -1,43 +1,49 @@
 
-for (var key in colorMap) {
-    if (colorMap.hasOwnProperty(key)) {
-        setCSSVariable(`${key}-color`, colorMap[key]);
+loadTable()
+function loadTable() {
+    for (var key in colorMap) {
+        if (colorMap.hasOwnProperty(key)) {
+            setCSSVariable(`${key}-color`, colorMap[key]);
+        }
+    }
+    var selector = document.getElementById("selector")
+    if (selector instanceof HTMLInputElement) {
+        var selectors = selector.value.split(' ');
+    }
+
+
+
+
+    var wrapper = document.getElementById("wrapper");
+    if (wrapper.childElementCount == 1) {
+        wrapper.removeChild(wrapper.firstChild)
+    }
+    var table = document.createElement('table');
+    wrapper.appendChild(table);
+    table.id = "outer";
+    var tbody = document.createElement('tbody');
+    table.appendChild(tbody);
+    {
+        var tr = document.createElement('tr');
+        tbody.appendChild(tr);
+        tr.appendChild(createMiscCell("Órarend"));
+    }
+    for (var i = 0; i < 9; i++) {
+        tr.appendChild(createMiscCell(i));
+    }
+    for (var i = 0; i < 5; i++) {
+        var tr = document.createElement('tr');
+        tbody.appendChild(tr);
+        tr.appendChild(createMiscCell(days[i]));
+        for (var j = 0; j < 9; j++) {
+            var tdTable = createLessonsCell(nyf[i][j]);
+            tr.appendChild(tdTable)
+        }
     }
 }
-var selector = document.getElementById("selector")
-if (selector instanceof HTMLInputElement) {
-    var selectors = selector.value.split(' ');
-}
 
 
 
-
-var wrapper = document.getElementById("wrapper");
-if (wrapper.childElementCount == 1) {
-    wrapper.removeChild(wrapper.firstChild)
-}
-var table = document.createElement('table');
-wrapper.appendChild(table);
-table.id = "outer";
-var tbody = document.createElement('tbody');
-table.appendChild(tbody);
-{
-    var tr = document.createElement('tr');
-    tbody.appendChild(tr);
-    tr.appendChild(createMiscCell("Órarend"));
-}
-for (var i = 0; i < 9; i++) {
-    tr.appendChild(createMiscCell(i));
-}
-for (var i = 0; i < 5; i++) {
-    var tr = document.createElement('tr');
-    tbody.appendChild(tr);
-    tr.appendChild(createMiscCell(days[i]));
-    for (var j = 0; j < 9; j++) {
-        var tdTable = createLessonsCell(nyf[i][j]);
-        tr.appendChild(tdTable)
-    }
-}
 function createLessonsCell(lessonArr) {
     var td = document.createElement('td');
     var table = document.createElement('table');
